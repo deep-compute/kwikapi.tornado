c4c2b70a975c7bc73a5c3525ef7d616ae51fd27c
Supriyo Paul <supriyo@deepcompute.com>
2018-10-30 07:18:12 +0100

    version 0.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # https://docs.djangoproject.com/en/1.11/intro/reusable-apps/
-version = '0.3.1'
+version = '0.3.2'
 setup(
     name="kwikapi-tornado",
     version=version,
